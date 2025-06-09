ae1e40bfd2a209f5259cf738a2cfa46f1c303bff
AptS-1547 <esaps@esaps.net>
2025-06-09 20:32:03 +0800

    fix: 更新 TypeScript 配置，启用自动导入和严格模式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,59 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/i18n'
-  ],
-
-  ui: {
-    icons: ['heroicons'],
-    fonts: false
+  imports: {
+    autoImport: true,
   },
 
-  i18n: {
-    locales: [
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxtjs/tailwindcss",
+    ["@nuxt/ui", { icons: ["heroicons"], fonts: false }],
+    [
+      "@nuxtjs/i18n",
       {
-        code: 'zh',
-        iso: 'zh-CN',
-        name: '简体中文',
-        file: 'zh.json'
+        locales: [
+          {
+            code: "zh",
+            iso: "zh-CN",
+            name: "简体中文",
+            file: "zh.json",
+          },
+          {
+            code: "en",
+            iso: "en-US",
+            name: "English",
+            file: "en.json",
+          },
+          {
+            code: "ja",
+            iso: "ja-JP",
+            name: "日本語",
+            file: "ja.json",
+          },
+        ],
+        lazy: true,
+        langDir: "locales/",
+        defaultLocale: "zh",
+        detectBrowserLanguage: {
+          useCookie: true,
+          cookieKey: "i18n_redirected",
+          redirectOn: "root",
+        },
+        bundle: {
+          optimizeTranslationDirective: false,
+        },
       },
-      {
-        code: 'en',
-        iso: 'en-US',
-        name: 'English',
-        file: 'en.json'
-      },
-      {
-        code: 'ja',
-        iso: 'ja-JP',
-        name: '日本語',
-        file: 'ja.json'
-      }
     ],
-    lazy: true,
-    langDir: 'locales/',
-    defaultLocale: 'zh',
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root'
-    },
-    bundle: {
-      optimizeTranslationDirective: false
-    }
-  }
-})
+  ],
+});
